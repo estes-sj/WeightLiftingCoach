84cbfbc0ec9c6824717eed1b40f8522d2a98a8e9
primetime728 <samuel.estes2000@gmail.com>
2022-03-03 00:02:12 -0500

    T6-76

---
+++ create_xml.py
@@ -0,0 +1,105 @@
+from xml.dom import minidom
+import os 
+from datetime import datetime
+import glob
+  
+def new_xml():
+    root = minidom.Document()
+    
+    xml = root.createElement('exercise')
+    xml.setAttribute('type', 'squat')
+    root.appendChild(xml)
+    
+    productChild = root.createElement('date')
+    xml.appendChild(productChild)
+    text = root.createTextNode(str(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+    productChild.appendChild(text)
+
+    productChild = root.createElement('score')
+    xml.appendChild(productChild)
+    productChild.appendChild(root.createTextNode("0"))
+
+    productChild = root.createElement('reps')
+    xml.appendChild(productChild)
+    productChild.appendChild(root.createTextNode("0"))
+
+    productChild = root.createElement('knee_angle_bottom')
+    xml.appendChild(productChild)
+    productChild.appendChild(root.createTextNode("0"))
+
+    productChild = root.createElement('back_angle_bottom')
+    xml.appendChild(productChild)
+    productChild.appendChild(root.createTextNode("0"))
+
+    productChild = root.createElement('feedback')
+    xml.appendChild(productChild)
+    productChild.appendChild(root.createTextNode("None"))
+
+    xml_str = root.toprettyxml(indent ="\t") 
+    
+    path_pattern = "data/squat_data_%s.xml"
+    #save_path_file = next_path(path_pattern) #used for next_path function
+    save_path_file = path_pattern % next_file_number()
+    print("New File Added: " + save_path_file)
+    
+    with open(save_path_file, "w") as f:
+        f.write(xml_str) 
+
+    print("Oldest File Removed: " + del_oldest_file())
+    #print(next_file())
+
+# Returns the next file number in sequence w/o replacements
+def next_file_number():
+    # Find newest file ID number
+    list_of_files = glob.glob('data/*') # * means all if need specific format then *.csv
+    #print(list_of_files)
+    latest_file = str(max(list_of_files, key=os.path.getmtime)) #getmtime/getctime
+    # Returns newest file ID number + 1
+    return int(latest_file[16:len(latest_file)-4])+1
+
+def del_oldest_file():
+    list_of_files = os.listdir('data')
+    full_path = ["data/{0}".format(x) for x in list_of_files]
+
+    if len(list_of_files) > 10:
+        oldest_file = min(full_path, key=os.path.getmtime)
+        os.remove(oldest_file)
+        return oldest_file
+
+"""     list_of_files = os.listdir('videos')
+    full_path = ["videos/{0}".format(x) for x in list_of_files]
+    if len(list_of_files) > 15:
+        oldest_file = min(full_path, key=os.path.getmtime)
+        os.remove(oldest_file)
+        return oldest_file """
+
+# O(logn) Returns the file path of the next squat with replacements considered
+def next_path(path_pattern):
+    """
+    Finds the next free path in an sequentially named list of files
+
+    e.g. path_pattern = 'file-%s.txt':
+
+    file-1.txt
+    file-2.txt
+    file-3.txt
+
+    Runs in log(n) time where n is the number of existing files in sequence
+    """
+    i = 1
+
+    # First do an exponential search
+    while os.path.exists(path_pattern % i):
+        i = i * 2
+
+    # Result lies somewhere in the interval (i/2..i]
+    # We call this interval (a..b] and narrow it down until a + 1 = b
+    a, b = (i // 2, i)
+    while a + 1 < b:
+        c = (a + b) // 2 # interval midpoint
+        a, b = (c, b) if os.path.exists(path_pattern % c) else (a, c)
+
+    return path_pattern % b
+
+if __name__ == '__main__':
+    new_xml()
--- squat_dictionary.py
+++ squat_dictionary.py
@@ -4,14 +4,14 @@ import os
 # Find newest file
 list_of_files = glob.glob('logs/*.log') # * means all if need specific format then *.csv
 print(list_of_files)
-latest_file = max(list_of_files, key=os.path.getctime)
-print(latest_file)
+latest_file = sorted(list_of_files, key=os.path.getmtime) #getmtime/getctime
+# Newest log
+print(latest_file[-1]) #-1 = newest, 0 = oldest
 
 
-""" # Delete oldest file
-list_of_files = os.listdir('log')
-full_path = ["log/{0}".format(x) for x in list_of_files]
+# Delete oldest file
+list_of_files = os.listdir('logs')
 
 if len(list_of_files) == 25:
-    oldest_file = min(full_path, key=os.path.getctime)
-    os.remove(oldest_file) """
+    oldest_file = min(list_of_files, key=os.path.getctime)
+    os.remove(os.path.abspath(oldest_file))
