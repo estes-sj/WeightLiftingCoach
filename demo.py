a686e0a7e90b4b3c295bcc394bebf9a854708e0b
primetime728 <samuel.estes2000@gmail.com>
2022-03-08 15:43:01 -0500

    fixes

--- demo.py
+++ demo.py
@@ -66,7 +66,7 @@ def main():
     while True:
         try:
 			# open streams for camera 0
-            camera = jetson.utils.videoSource('videos/IMG_2826.MP4', argv=["--input-width=1280 --input-height=720"])      # '/dev/video0' for V4L2 
+            camera = jetson.utils.videoSource('videos/IMG_2826.avi', argv=["--input-width=1280 --input-height=720"])      # '/dev/video0' for V4L2 
             #camera = jetson.utils.videoSource("csi://0", argv=["--input-flip=rotate-180"])      # '/dev/video0' for V4L2 
             #display = jetson.utils.videoOutput('display://0') # 'my_video.mp4' for file
             display = jetson.utils.videoOutput('videos/IMG_2826_RESULTS.mp4', argv=["--output-width=1280 --output-height=720"]) # 'my_video.mp4' for file
